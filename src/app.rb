95b46987877c44dfcc9320602ec535742d9cfa98
keyasuda <keyasuda@users.noreply.github.com>
2022-05-08 23:16:29 +0900

    地名検索結果をSessionStorageに投入できるように

--- src/app.rb
+++ src/app.rb
@@ -1,22 +1,30 @@
 require 'sinatra/base'
+require 'bormashino/local_storage'
+require_relative './gsi_api'
 
 class App < Sinatra::Base
   set :protection, false
 
+  before do
+    @cache = Bormashino::SessionStorage.instance
+  end
+
+  # クエリパラメータ
+  # q=検索クエリ文字列
+  # p=強調表示すべき地点のindex
   get '/' do
-    @world = 'world'
-    erb :index
+    if params[:q] && !@cache.get_item(params[:q])
+      GsiApi.address_search(params[:q], '/address_callback', params)
+      'qued'
+    else
+      'not qued'
+    end
   end
 
-  get '/link' do
-    'link clicked'
-  end
+  post '/address_callback' do
+    options = JSON.parse(params[:options])
+    @cache.set_item(options['q'], params[:payload])
 
-  put '/form-submit' do
-    "You put: #{params[:value]}"
-  end
-
-  delete '/destroy' do
-    '2nd form has submitted'
+    redirect to("/?q=#{CGI.escape(options['q'])}&p=#{options['p']}")
   end
 end
---
+++ src/gsi_api.rb
@@ -0,0 +1,12 @@
+require 'cgi'
+require 'bormashino/fetch'
+
+module GsiApi
+  def self.address_search(query, callback_to, options)
+    Bormashino::Fetch.new(
+      resource: "https://msearch.gsi.go.jp/address-search/AddressSearch?q=#{CGI.escape(query)}",
+      resolved_to: callback_to,
+      options:,
+    ).run
+  end
+end
