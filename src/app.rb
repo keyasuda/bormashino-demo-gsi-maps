ceec3f3cdd2c09ab2bfe9f6ca95a6a17724daf66
keyasuda <keyasuda@users.noreply.github.com>
2022-05-09 23:41:55 +0900

    地名検索結果を表示できるように

--- src/app.rb
+++ src/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'bormashino/local_storage'
+require 'json/pure'
 require_relative './gsi_api'
 
 class App < Sinatra::Base
@@ -13,18 +14,20 @@ class App < Sinatra::Base
   # q=検索クエリ文字列
   # p=強調表示すべき地点のindex
   get '/' do
+    @loading = false
     if params[:q] && !@cache.get_item(params[:q])
       GsiApi.address_search(params[:q], '/address_callback', params)
-      'qued'
-    else
-      'not qued'
+      @loading = true
     end
+    @pois = JSON.parse(@cache.get_item(params['q']) || '[]')
+
+    erb :index
   end
 
   post '/address_callback' do
     options = JSON.parse(params[:options])
     @cache.set_item(options['q'], params[:payload])
 
-    redirect to("/?q=#{CGI.escape(options['q'])}&p=#{options['p']}")
+    redirect to("/?q=#{CGI.escape(options['q'])}")
   end
 end
