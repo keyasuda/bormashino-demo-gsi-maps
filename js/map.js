2e2aee157cd51e83eae2547fadb787a8f25df025
keyasuda <keyasuda@users.noreply.github.com>
2022-05-10 23:17:28 +0900

    地点リストをクリックするとその地点のマーカーの色を変えて地図を移動するように

--- js/map.js
+++ js/map.js
@@ -1,12 +1,8 @@
 import L, { Icon } from 'leaflet'
-delete Icon.Default.prototype._getIconUrl
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-})
+import 'leaflet-extra-markers'
 
 export const showMap = (container, app) => {
+  // Leafletの初期化
   const map = L.map(container)
   L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
     attribution:
@@ -15,15 +11,33 @@ export const showMap = (container, app) => {
   map.setView([35, 135], 5)
   const markerLayer = L.layerGroup().addTo(map)
 
+  // Ruby appが画面を更新した(=地点一覧が更新された)ら地図を更新する
   app.addEventListener('bormashino:updated', (e) => {
     markerLayer.clearLayers()
 
-    const markers = Array.from(e.target.querySelectorAll('li')).map((li) => {
+    // ul > li からマーカーを生成する
+    const markers = Array.from(e.target.querySelectorAll('li')).map((li, i) => {
       const g = JSON.parse(li.dataset.geometry)
-      return L.marker(g.coordinates.reverse()).addTo(markerLayer)
+      let color = 'blue'
+      if (li.className == 'selected') color = 'red'
+      return L.marker(g.coordinates.reverse(), {
+        icon: L.ExtraMarkers.icon({
+          icon: 'fa-number',
+          number: i + 1,
+          markerColor: color,
+        }),
+      }).addTo(markerLayer)
     })
 
-    const group = L.featureGroup(markers)
-    map.fitBounds(group.getBounds())
+    const selected = e.target.querySelector('li.selected')
+    if (selected) {
+      // 選択された地点があればその位置に移動
+      const g = JSON.parse(selected.dataset.geometry)
+      map.setView(g.coordinates.reverse(), 15)
+    } else {
+      // 全体が見えるように
+      const group = L.featureGroup(markers)
+      map.fitBounds(group.getBounds())
+    }
   })
 }
